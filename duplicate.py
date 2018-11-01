376d8a6b24e11fb48af11b6367fedac700ae6867
drishya123 <drishya16manohar@gmail.com>
2018-11-01 13:47:12 +0530

    initial commit

--- duplicate.py
+++ duplicate.py
@@ -1,11 +1,15 @@
-l = [1, 1, 2, 2, 3, 3]
+l = [1, 1, 2, 2, 2, 2, 3]
+k = []
+q = []
+
+for data in l:
+	if data not in k:
+		k.append(data)
+		q.append(l.count(data))
 count = 0
-for i in l:
-	for j in l:
+for j in q:
+	if(q[j] % 2 == 0):
+		count = count + 1
+print(q)
 		
-		if(i == j):		
-			count = count + 1
-		else:
-			count = count
-print(count)
 

3a9a776baad8d1fa258596001c8ac6db21d633e4
drishya123 <drishya16manohar@gmail.com>
2018-11-01 11:39:56 +0530

    initial commit

---
+++ printarray.py
@@ -0,0 +1,10 @@
+class Print:
+     def __init__(self, value):
+            self.value=value
+     def printarray(self):
+             t=self.value.split(" ")
+             print(t)
+s=input()
+s=Print(s)
+s.printarray()
+
